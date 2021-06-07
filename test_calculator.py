be58cd2509a5247c0755d751b0dba4302191756c
JPChaus <48576328+JPChaus@users.noreply.github.com>
2021-06-07 00:10:14 -0400

    fixed linting errors and added multiply code

--- test_calculator.py
+++ test_calculator.py
@@ -6,12 +6,10 @@ import calculator
 
 class Test_CalculatorApp:
     def test_add(self):
-        assert 5 == calculator.add(3,2)
-
+        assert 5 == calculator.add(3, 2)
 
     def test_subtract(self):
-        assert 1 == calculator.subtract(3,2)
+        assert 1 == calculator.subtract(3, 2)
 
-    
     def test_multiply(self):
         assert 20 == calculator.multiply(4, 5)
