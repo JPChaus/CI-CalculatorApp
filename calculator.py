bde800855af9f0d26a48189ed2a8cc3087be6422
JPChaus <48576328+JPChaus@users.noreply.github.com>
2021-06-06 23:55:25 -0400

    added comfig file for circleci

--- calculator.py
+++ calculator.py
@@ -1,11 +1,11 @@
 """
-Jackson E. Rollins 6/6/2021
-
-calculator app using circle ci
+Jackson E. Rollins 6/6/2021 - calculator app using circle ci
 """
+
 
 def add(x, y):
     return x + y
+
 
 def subtract(x, y):
     return x - y
